0628a48542779f2262cdbe775e88bea193b7bf8b
Prashant30Pandey <49170173+Prashant30Pandey@users.noreply.github.com>
2020-12-04 18:18:43 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -1,12 +1,12 @@
 $(window).load(function(){
-	$('.loading').fadeOut(1500);
-	$('.container').fadeIn(1500);
+	$('.loading').fadeOut(2000);
+	$('.container').fadeIn(2000);
 });
 $('document').ready(function(){
 		var vw;
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
-			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
+			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7,#b8,#b9').stop();
 			$('#b11').animate({top:240, left: vw-350},500);
 			$('#b22').animate({top:240, left: vw-250},500);
 			$('#b33').animate({top:240, left: vw-150},500);
@@ -14,6 +14,8 @@ $('document').ready(function(){
 			$('#b55').animate({top:240, left: vw+50},500);
 			$('#b66').animate({top:240, left: vw+150},500);
 			$('#b77').animate({top:240, left: vw+250},500);
+			$('#b88').animate({top:240, left: vw+350},500);
+			$('#b99').animate({top:240, left: vw+450},500);
 		});
 
 	$('#turn_on').click(function(){
